38ce189c960f21f353b213f859212ecc958b39cf
Mark <me@jsman.ru>
2013-03-19 23:53:13 +0200

    demo: better nav list

--- tests/www/js/app/demo/NavItem.js
+++ tests/www/js/app/demo/NavItem.js
@@ -4,11 +4,29 @@ define(function(require) {
         ATTR_caption : {
             change : '_uiSet_caption'
         },
+
+        ATTR_active : {
+            change : '_uiSet_active'
+        },
+
+        ATTR_href : {
+            change : '_uiSet_href'
+        },
+
         render : function () {
             return BaseClass.prototype.render.call(this);
         },
+
         _uiSet_caption : function (name, value) {
             this.byName('caption').set('text', value);
+        },
+
+        _uiSet_active : function (name, value) {
+            this.byName('caption').set('css.active', value);
+        },
+
+        _uiSet_href : function (name, value) {
+            this.byName('caption').set('attr.href', value);
         }
     });
 });
